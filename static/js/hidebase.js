599927245dca4813c703395ef377a95c37a3e521
KRyan23 <GitHub@kryan23.bulc.club>
2022-02-25 14:25:35 +0000

    changed main carousel look

--- static/js/hidebase.js
+++ static/js/hidebase.js
@@ -1,4 +1,4 @@
 console.log("testing js load");
 
 /* Hide main content on template load */
-  $( "#hideMainContent" ).addClass( 'hide-main-page-content');
+  $( "#hideMainContent" ).addClass( 'invisible');
