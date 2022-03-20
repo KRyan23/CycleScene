958d5dea4c642eea7eded44f223d3120f233a330
KRyan23 <GitHub@kryan23.bulc.club>
2022-03-20 21:13:38 +0000

    added add to bag basic functionality

--- shoppingbag/views.py
+++ shoppingbag/views.py
@@ -7,3 +7,21 @@ def view_shoppingbag(request):
 
     
     return render(request, 'shoppingbag/shoppingbag.html')
+
+def add_product_to_shoppingbag(request, item_id):
+    ''' Adds products to shopping bag function '''
+    
+    bag_quantity = request.POST.get('quantity')
+    print(bag_quantity)
+    bag_quantity = int(bag_quantity)
+    current_page = request.POST.get('current_page_url')
+    shopping_bag = request.session.get('shopping_bag', {})
+
+    if item_id in list(shopping_bag.keys()):
+       shopping_bag[item_id] += bag_quantity
+    else:
+        shopping_bag[item_id] = bag_quantity
+
+    request.session['shopping_bag'] = shopping_bag
+    print(request.session['shopping_bag'])
+    return redirect(current_page)
--- static/js/quantity.js
+++ static/js/quantity.js
@@ -1,24 +1,26 @@
 /* Script to increment and decrement the quantity value in the products details 
 'add to bag' input box hard min and max set to 1 and 10 repectively */
 
+
 function increment(){
-    var bagvalue = $('#bag-quantity').val();
+    var bagvalue = $('.form-quantity').val();
     bagvalue = Number(bagvalue);
     if (bagvalue === 10){
         bagvalue = 10
     }else{
         bagvalue = bagvalue + 1;
     }
-    document.getElementById('bag-quantity').value=bagvalue; 
+    document.getElementsByClassName('form-quantity')[0].value = bagvalue;
+   
 }
 
 function decrement(){
-    var bagvalue = $('#bag-quantity').val();
+    var bagvalue = $('.form-quantity').val();
     bagvalue = Number(bagvalue);
     if (bagvalue === 1) {
         bagvalue = 1;
     }else{
         bagvalue = bagvalue - 1;
     }
-    document.getElementById('bag-quantity').value=bagvalue; 
+    document.getElementsByClassName('form-quantity')[0].value = bagvalue;
 }
