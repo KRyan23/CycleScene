3da000a0de347c1f3027eb8a42810114b6739c63
KRyan23 <GitHub@kryan23.bulc.club>
2022-03-26 14:09:39 +0000

    added +/- button functionality in shoppingbag

--- static/js/quantityShoppingBag.js
+++ static/js/quantityShoppingBag.js
@@ -1,26 +1,36 @@
-/* Change the quantity on each shopping bag item*/
+/* Script to increment and decrement the quantity value in the products details 
+'add to bag' input box hard min and max set to 1 and 10 repectively + max and min messages*/
 
-$('.change-product-quantity').click(function (e) {
-    var readFormQuantity = $(this).prev('.shopping-bag-form');
-    readFormQuantity.submit();
-})
+$(document).ready(function(){
+    $(".increment-value").click(function() {
+        var dataId = $(this).attr("id");
+        var thenum = dataId.replace( /^\D+/g, '');
+        var inputFieldValue = document.getElementById('id_qty_'+thenum).value;
+        inputFieldValue = Number(inputFieldValue);        
+        if (inputFieldValue === 10){
+            inputFieldValue = 10
+            document.getElementById('error_id_'+thenum).innerHTML="Hi items are limited to 10 / Order";
+        }else{
+            inputFieldValue++;
+            document.getElementById('error_id_'+thenum).innerHTML="";
+        }
+        document.getElementById('id_qty_'+thenum).value = inputFieldValue;
+    });
+});
 
-
-$('.remove-item').click(function(e) {
-    console.log("1");
-    var csrfToken = "{{ csrf_token }}";
-    console.log("2");
-    var itemId = $(this).attr('id');
-    console.log(itemId);
-    
-    console.log("3");
-    var url = `/shoppingbag/remove/${itemId}/`;
-    console.log("4");
-    var data = {'csrfmiddlewaretoken': csrfToken };
-    console.log("5");
-    $.post(url, data)
-     .done(function() {
-         location.reload();
-     });
-})
-
+$(document).ready(function(){
+    $(".decrement-value").click(function() {
+        var dataId = $(this).attr("id");
+        var thenum = dataId.replace( /^\D+/g, '');
+        var inputFieldValue = document.getElementById('id_qty_'+thenum).value;
+        inputFieldValue = Number(inputFieldValue);        
+        if (inputFieldValue === 1 ){
+            inputFieldValue = 1
+            document.getElementById('error_id_'+thenum).innerHTML="Hi the minimum order is 1!";
+        }else{
+            inputFieldValue--;
+            document.getElementById('error_id_'+thenum).innerHTML="";
+        }
+        document.getElementById('id_qty_'+thenum).value = inputFieldValue;
+    });
+});
