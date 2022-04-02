cd87be3235c17a46e110b3dd3535553d4fb06973
KRyan23 <GitHub@kryan23.bulc.club>
2022-04-02 20:18:34 +0000

    fixed totals issue in checkout.html

--- shoppingbag/contexts.py
+++ shoppingbag/contexts.py
@@ -3,15 +3,18 @@ from decimal import Decimal
 from django.conf import settings
 from django.shortcuts import get_object_or_404
 from products.models import Product
+from django.contrib import messages
 
 def shoppingbag_contents(request):
 
     ''' Handles the shoppingbag requests '''
     shoppingbag_items = []
     bagtotal = 0
+    total_bag_cost = 0
     product_total = 0
     product_count = 0
     free_delivery = 1
+    delivery = 0
     mybag = request.session.get('shopping_bag', {})
 
     for item_id, quantity in mybag.items():
@@ -25,23 +28,19 @@ def shoppingbag_contents(request):
             })
 
     if bagtotal > settings.FREE_DELIVERY_DELTA:
-        delivery_local = 0
-        delivery_pickup = 0
-        free_delivery = 0
+        total_bag_cost = bagtotal * free_delivery
     else:
-        delivery_local = bagtotal + Decimal(settings.STANDARD_DELIVERY_COST)
-        delivery_pickup = bagtotal * Decimal(settings.LOCAL_PICKUP_COST)
-        #total_bag_cost = bagtotal + delivery_local + delivery_pickup + free_delivery
-
+        total_bag_cost = bagtotal + Decimal(settings.STANDARD_DELIVERY_COST)
+        delivery = delivery + Decimal(settings.STANDARD_DELIVERY_COST)
+       
     context = {
                 'shoppingbag_items': shoppingbag_items,
                 'bagtotal': bagtotal,
                 'product_total': product_total,
                 'product_count': product_count,
-                'delivery_local': delivery_local,
-                'delivery_pickup': delivery_pickup,
                 'free_delivery': free_delivery,
-                #'total_bag_cost': total_bag_cost,
+                'total_bag_cost': total_bag_cost,
                 'free_delivery_delta': settings.FREE_DELIVERY_DELTA,
+                'delivery': delivery,
             }
     return context
