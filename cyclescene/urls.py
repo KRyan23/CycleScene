b86ff274678d5f0bb47da6b00b30b78d76c14dac
KRyan23 <GitHub@kryan23.bulc.club>
2022-04-28 19:59:45 +0000

    modified files for custom error pages

--- cyclescene/urls.py
+++ cyclescene/urls.py
@@ -1,24 +1,10 @@
-"""cyclescene URL Configuration
+'''cyclescene URL Configuration'''
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 import os
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -31,3 +17,8 @@ urlpatterns = [
     path('service/', include('service.urls')),
     path('profile/', include('profiles.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+handler404 = "cyclescene.views.page_not_found"
+handler400 = "cyclescene.views.bad_request"
+handler500 = "cyclescene.views.internal_server_error"
